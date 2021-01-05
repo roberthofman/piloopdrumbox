9375d9f6635a685cbc5902b7fd2a57f93a075e02
roberthofman <robert10321@gmail.com>
2021-01-05 21:31:51 +0100

    fix

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main():
     PORT_SEND_TO_PD = 3000
 
     # initiate python to PD class
-    send_msg = Py_to_pd(PD_PATH, PORT_SEND_TO_PD, mac)
+    send_msg = Py_to_pd(PD_PATH, PORT_SEND_TO_PD)
 
     # start the socket (Pd_to_py)
     os.system('python3 Pd_to_py.py &')
