6e90a2772d3e59c3b3b620a9c7e66350caf2f30c
roberthofman <robert10321@gmail.com>
2021-01-08 16:26:40 +0100

    fix and final push for test

--- Button_pad.py
+++ Button_pad.py
@@ -18,15 +18,16 @@ class Button_pad:
         self.NUM_COLORS = 1
         # Vary this number if the key press is not registered correctly
         # It basically sets the sensitivity of the button (press/no press)
-        self.MAX_DEBOUNCE = 3
+        self.MAX_DEBOUNCE = 3 # should range between 2-3 accorinding to Sparkfun
         # Global Variables
         self.LED_buffer = self.create_matrix(False, self.NUM_LED_COLUMNS, self.NUM_LED_ROWS)
-        self.btnColumnPins = [] # Pin numbers for columns
-        self.btnRowPins = [] # Pin numbers for rows
-        self.ledColumnPins = [] # Pin numbers for the LED's (columns)
-        self.colorPins = [] # Pin numbers for LED rows
-        #
-        self.debounce_count = self.create_matrix(0, self.NUM_BTN_COLUMNS, NUM_BTN_ROWS)
+        self.btnColumnPins = [] # Pin numbers for columns (4)
+        self.btnRowPins = [] # Pin numbers for rows (4)
+        self.ledColumnPins = [] # Pin numbers for the LED's (columns) (4)
+        self.colorPins = [] # Pin numbers for LED rows (4)
+        # Tracks how often a button is pressed
+        self.debounce_count = self.create_matrix(0, self.NUM_BTN_COLUMNS, self.NUM_BTN_ROWS)
+        self.button_timer = self.create_matrix(0, self.NUM_BTN_COLUMNS, self.NUM_BTN_ROWS)
 
     def create_matrix(self, value, y_range, x_range):
         """
@@ -56,42 +57,46 @@ class Button_pad:
             GPIO.setup(self.colorPins[row], GPIO.OUT, initial=GPIO.LOW)
 
     def scan(self):
+        """
+        Function to be looped to scan button presses
+        It quickly goes over all columns (current) and corresponding rows
+        """
         for current in range(self.NUM_LED_COLUMNS):
             #Select current columns -> start without LED
-            GPIO.output(btnColumnPins[current], GPIO.LOW)
-            GPIO.output(ledColumnPins[current], GPIO.LOW)
+            GPIO.output(self.btnColumnPins[current], GPIO.LOW)
+            GPIO.output(self.ledColumnPins[current], GPIO.LOW)
 
             # output LED row values
             for row in self.NUM_LED_ROWS:
                 if(self.LED_buffer[current][row]):
-                    GPIO.output(colorPins[row], GPIO.HIGH)
+                    GPIO.output(self.colorPins[row], GPIO.HIGH)
 
-            time.wait(1/1000)
+            time.sleep(1/1000)
 
             # read the button inputs
             for row in self.NUM_BTN_ROWS:
-                val = GPIO.input(btnRowPins[row])
+                val = GPIO.input(self.btnRowPins[row])
                 if val == GPIO.LOW:
                     # Active low: val is low when btn is pressed
-                    if debounce_count[current][row] < MAX_DEBOUNCE]:
-                        debounce_count[current][row] += 1
-                        if debounce_count[current][row] == MAX_DEBOUNCE:
+                    if self.debounce_count[current][row] < self.MAX_DEBOUNCE]:
+                        self.debounce_count[current][row] += 1
+                        if self.debounce_count[current][row] == MAX_DEBOUNCE:
                             print("Key Down: " + current*self.NUM_BTN_ROWS + ", " + row)
                             #Send button press
-                            LED_buffer[current][j] = !LED_buffer[current][j]
+                            self.LED_buffer[current][j] = !self.LED_buffer[current][j]
                 else:
                     # Button is released
-                    if debounce_count[current][j] > 0:
-                        debounce_count[current][j] -= 1
+                    if self.debounce_count[current][j] > 0:
+                        self.debounce_count[current][j] -= 1
                         if debounce_count[current][j] == 0:
                             print("Key Up: " + current*self.NUM_BTN_ROWS + ", " + row)
                             #Send key release
 
-            time.wait(1/1000)
+            time.sleep(1/1000)
 
             # Reset button to init value
-            GPIO.output(btnColumnPins[current], GPIO.HIGH)
-            GPIO.output(ledColumnPins[current], GPIO.HIGH)
+            GPIO.output(self.btnColumnPins[current], GPIO.HIGH)
+            GPIO.output(self.ledColumnPins[current], GPIO.HIGH)
 
             for row in self.NUM_LED_ROWS:
-                GPIO.output(colorpins[row], GPIO.LOW)
+                GPIO.output(self.colorPins[row], GPIO.LOW)
