9eb936c6f621e9ef52b0dc03fce4e38be218a2dc
roberthofman <robert10321@gmail.com>
2021-01-09 21:10:54 +0100

    LCD test script

---
+++ lcd.py
@@ -0,0 +1,12 @@
+import RPi_I2C_driver
+import time
+
+while True:
+    mylcd = RPi_I2C_driver.lcd()
+    mylcd.lcd_display_string("Peet soldeer",1)
+    mylcd.lcd_display_string("Robert kijkt",2)
+    print("Text should show up")
+    time.sleep(2)
+    mylcd.lcd_clear()
+    print("lcd should be cleared")
+    time.sleep(2)
